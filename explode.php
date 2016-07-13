47904929ea7898b367585ee027b98e98e6c65fa9
David Larrimore <davidlarrimore@gmail.com>
2016-07-12 23:35:10 -0400

    Init commit
    Restoring basic Gulp scripts and functionality

---
+++ explode.php
@@ -0,0 +1,62 @@
+<?php
+
+$unpack = false;
+
+function Delete($path)
+{
+    if (is_dir($path) === true)
+    {
+        $files = array_diff(scandir($path), array('.', '..'));
+        foreach ($files as $file)
+        {
+            Delete(realpath($path) . '/' . $file);
+        }
+        return rmdir($path);
+    }
+    else if (is_file($path) === true){return unlink($path);}
+    return false;
+}
+
+echo "<h3>Deleting Existing Files</h3>";
+$dir = new DirectoryIterator(dirname(__FILE__));
+foreach ($dir as $fileinfo) {
+	if ($fileinfo->getFilename() != "public_html.zip" && $fileinfo->getFilename() != "explode.php"){
+	    if (is_file($fileinfo)) {
+	        echo 'Deleted File: '.$fileinfo->getFilename()."</br>";
+	        unlink($fileinfo);
+	    }else if (is_dir($fileinfo)){
+	    	if ($fileinfo->getFilename() != "."  && $fileinfo->getFilename() != ".."){
+	    		echo 'Deleted Directory: '.$fileinfo->getFilename()."</br>";
+	    		Delete($fileinfo);	    	
+	    	}	
+	    }	
+	}
+}
+
+echo "<hr/>";
+
+echo "<h3>Attempting to Extract public_html.zip</h3>";
+$zip = new ZipArchive;
+if ($zip->open('public_html.zip') === TRUE) {
+    $zip->extractTo(getcwd());
+    $zip->close();
+    echo '<h3>public_html.zip extraction successful</h3><br/>';
+    $unpack = true;
+} else {
+    echo '<h3>public_html.zip extraction FAILED</h3></br>';
+}
+
+
+if($unpack){
+echo "<hr/>";
+
+echo "<h3>Removing exploder and .zip</h3>";
+
+unlink('explode.php');
+unlink('public_html.zip');
+}
+
+
+echo "<h3>Completed!</h3>";
+
+?>
---
+++ gulptest.js
@@ -0,0 +1,111 @@
+var gulp = require('gulp');
+const del = require('del');
+var git = require('gulp-git');
+var config = require('config');
+var ftp = require( 'vinyl-ftp' );
+const zip = require('gulp-zip');
+
+
+
+var remotePath = '/dev2/';
+
+var conn = ftp.create( {
+    host: config.get('ftp.host'),
+    port: config.get('ftp.port'),
+    user: config.get('ftp.username'),
+    password: config.get('ftp.password'),
+    parallel: 5
+} );
+
+
+
+
+gulp.task('clean', function() {
+	return del(['working/']);
+});
+
+
+
+
+
+
+
+gulp.task('clone-html', ['clean'], function(cb) {
+	var stream = git.clone('https://github.com/COCAFoundation/public_html.git', {args: './working/public_html'}, function (err) {
+		if (err){throw err;}else{cb();}
+	 });
+	return stream; // return the stream as the completion hint
+});
+
+
+
+
+
+
+gulp.task('clone-template', ['clean'], function(cb) {
+	var stream = git.clone('https://github.com/COCAFoundation/coca_template.git', {args: './working/public_html/templates/coca'}, function (err) {
+		if (err){throw err;}else{cb();}
+	 });
+	return stream; // return the stream as the completion hint
+});
+
+
+
+
+
+gulp.task('zip', ['build'], function(){
+	var stream = gulp.src('working/public_html/**')
+		.pipe(zip('public_html.zip'))
+		.pipe(gulp.dest('working/'));
+	return stream; // return the stream as the completion hint
+});
+
+gulp.task('build', ['clone-html','clone-template'], function(cb){
+	gulp.src('./explode.php').pipe(gulp.dest('./working'));
+	cb();
+});
+
+
+
+
+
+
+
+
+
+gulp.task('transfer', ['build','zip'], function() {
+	console.log("Tranferring Files")
+
+	var globs = [
+	    'working/explode.php',
+	    'working/public_html.zip'
+	];  
+
+
+	return gulp.src( globs, { base: './working/', buffer: false } )
+		.pipe( conn.newer( remotePath ) ) // only upload newer files 
+		.pipe( conn.dest( remotePath ) )
+
+});
+
+
+
+
+gulp.task('delete-htaccess', ['transfer'],function() {
+	return conn.delete('/dev2/.htaccess', function () {});
+});
+
+
+gulp.task('explode', ['delete-htaccess'],function() {
+
+});
+
+
+
+
+gulp.task('default', ['build', 'zip', 'transfer', 'delete-htaccess', 'explode', ],function() {
+	//gulp.src('../coca_template/*').pipe(gulp.dest('./working/coca_template'));
+	//gulp.src('../public_html/*').pipe(gulp.dest('./working/public_html'));
+	//gulp.src('../coca_template/*').pipe(gulp.dest('./working/public_html/templates/coca'));
+	//console.log("I am Gulping bitches!!!!")
+});
